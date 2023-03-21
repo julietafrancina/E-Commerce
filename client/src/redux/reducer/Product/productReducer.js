b2a2f685de1d051467328051bce00f0c9598c721
Lila Bergamini <lila.bergamini@hotmail.com>
2023-03-21 09:17:05 -0300

    productReducer clean

--- client/src/redux/reducer/Product/productReducer.js
+++ client/src/redux/reducer/Product/productReducer.js
@@ -26,42 +26,6 @@ export const productReducer = (state = initialState, action) => {
         currentProduct: action.payload,
       };
 
-    // case types.ADD_PRODUCT_CART:
-    //   if (action.payload.length > 0) {
-    //     return {
-    //       ...state,
-    //       productsCart: action.payload,
-    //     };
-    //   } else {
-    //     let { id, count } = action.payload;
-    //     const exist = state.productsCart.some((p) => p.id === id);
-    //     if (exist) {
-    //       let newProductCart = state.productsCart.map((p) => {
-    //         if (p.id === id) {
-    //           return {
-    //             id: p.id,
-    //             count: p.count + count,
-    //             image: p.image,
-    //             name: p.name,
-    //             price: p.price,
-    //             priceTotal: p.price * (p.count + count),
-    //           };
-    //         } else {
-    //           return p;
-    //         }
-    //       });
-    //       return {
-    //         ...state,
-    //         productsCart: newProductCart,
-    //       };
-    //     } else {
-    //       return {
-    //         ...state,
-    //         productsCart: [...state.productsCart, action.payload],
-    //       };
-    //     }
-    //   }
-
     default:
       return state;
   }
